ba6b468d4e31282c301c5ba8695b6a23011aebb0
Leonardo Henrique <leonardojose@netcabo.pt>
2024-04-18 22:01:23 +0100

    MilitaryUnit Changes

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -4,11 +4,24 @@ namespace GameUnits
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
+        public override int Health { get => base.Health + XP;
+            set => base.Health = value; }
+
+        public override float Cost { get => AttackPower + XP;}
 
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
+        }
+
+        public void Attack(Unit u){
+            XP++;
+            u.Health -= AttackPower;
+        }
+
+        public override string ToString(){
+            return base.ToString() + $" AP={AttackPower} XP={XP}";
         }
     }
 }
